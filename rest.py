e64027b0a1935f679cbfff244a3c80670ffd3a30
panchishin <anton@panchishin.com>
2017-07-31 11:08:27 -0400

    just code spacing

--- rest.py
+++ rest.py
@@ -19,22 +19,22 @@ embeddings.data_set = mnist.test.images
 
 
 def getImageWithIndex(index) :
-    return mnist.test.images[index:index+1]
+  return mnist.test.images[index:index+1]
 
 def getExample(index,layer) :
-    return session.sess.run(layer,feed_dict={model.x0:getImageWithIndex(index)} ).reshape([model.SIZE,model.SIZE])
+  return session.sess.run(layer,feed_dict={model.x0:getImageWithIndex(index)} ).reshape([model.SIZE,model.SIZE])
 
 def arrayToImage(data) :
-    import scipy.misc
-    import tempfile
-    with tempfile.TemporaryFile() as fp :
-        scipy.misc.toimage( data ).save( fp=fp, format="PNG" )
-        fp.seek(0)
-        return fp.read()
+  import scipy.misc
+  import tempfile
+  with tempfile.TemporaryFile() as fp :
+    scipy.misc.toimage( data ).save( fp=fp, format="PNG" )
+    fp.seek(0)
+    return fp.read()
 
 def falconRespondArrayAsImage(data,resp) :
-    resp.content_type = 'image/png'
-    resp.body = arrayToImage(data)
+  resp.content_type = 'image/png'
+  resp.body = arrayToImage(data)
 
 
 print """
@@ -44,84 +44,84 @@ Define the rest endpoints
 """
 
 class Display:
-    def on_get(self, req, resp, file_name):
-        if not os.path.isfile("view/"+file_name) :
-            return
+  def on_get(self, req, resp, file_name):
+    if not os.path.isfile("view/"+file_name) :
+      return
 
-        result = open("view/"+file_name,"r")
-        if ( "html" in file_name) :
-            resp.content_type = "text/html"
-        else :
-            resp.content_type = "text/plain"
-        
-        resp.body = result.read()
-        result.close()
+    result = open("view/"+file_name,"r")
+    if ( "html" in file_name) :
+      resp.content_type = "text/html"
+    else :
+      resp.content_type = "text/plain"
+    
+    resp.body = result.read()
+    result.close()
 
 
 
 class LayerImage:
-    def on_get(self, req, resp, layer, index, junk) :
-      try :
-        ml_layer = [model.x_noisy,model.x_out_1,model.x_out_2,model.x_out_3,model.x_out_4,model.x_out_5,model.x_in][int(layer)]
-        falconRespondArrayAsImage( 
-          getExample(int(index),ml_layer) , 
-          resp 
-          )
-      except :
-        pass
+  def on_get(self, req, resp, layer, index, junk) :
+    try :
+      ml_layer = [model.x_noisy,model.x_out_1,model.x_out_2,model.x_out_3,model.x_out_4,model.x_out_5,model.x_in][int(layer)]
+      falconRespondArrayAsImage( 
+        getExample(int(index),ml_layer) , 
+        resp 
+        )
+    except :
+      pass
 
 
 class BlendImage:
-    def on_get(self, req, resp, a_value, b_value, amount) :
-      try :
-        amount = int(amount) / 100.0
-        a_embed = session.sess.run(model.conv5e,feed_dict={model.x0:getImageWithIndex(int(a_value))} )
-        b_embed = session.sess.run(model.conv5e,feed_dict={model.x0:getImageWithIndex(int(b_value))} )
-        blend_embed = a_embed * amount + b_embed * ( 1 - amount )
-        output = session.sess.run(model.x_out_5,feed_dict={model.conv5e:blend_embed,model.x0:getImageWithIndex(int(a_value))} )
-        falconRespondArrayAsImage( output.reshape([model.SIZE,model.SIZE]) , resp )
-      except :
-        pass
+  def on_get(self, req, resp, a_value, b_value, amount) :
+    try :
+      amount = int(amount) / 100.0
+      a_embed = session.sess.run(model.conv5e,feed_dict={model.x0:getImageWithIndex(int(a_value))} )
+      b_embed = session.sess.run(model.conv5e,feed_dict={model.x0:getImageWithIndex(int(b_value))} )
+      blend_embed = a_embed * amount + b_embed * ( 1 - amount )
+      output = session.sess.run(model.x_out_5,feed_dict={model.conv5e:blend_embed,model.x0:getImageWithIndex(int(a_value))} )
+      falconRespondArrayAsImage( output.reshape([model.SIZE,model.SIZE]) , resp )
+    except :
+      pass
 
 
 class DoLearning:
-    def on_get(self, req, resp, index) :
-        print "TRAINING WITH",index
-        session.doEpochOfTraining(
-          [model.loss_1,model.loss_2,model.loss_3,model.loss_4,model.loss_5,model.loss_6][int(index)],
-          [model.train_1,model.train_2,model.train_3,model.train_4,model.train_5,model.train_6][int(index)],
-          mnist.train)
-        embeddings.reset()
-        resp.body = json.dumps( { 'response': 'done'} )
+  def on_get(self, req, resp, index) :
+    print "TRAINING WITH",index
+    session.doEpochOfTraining(
+      [model.loss_1,model.loss_2,model.loss_3,model.loss_4,model.loss_5,model.loss_6][int(index)],
+      [model.train_1,model.train_2,model.train_3,model.train_4,model.train_5,model.train_6][int(index)],
+      mnist.train)
+    embeddings.reset()
+    resp.body = json.dumps( { 'response': 'done'} )
 
 class ResetSession:
-    def on_get(self, req, resp) :
-      session.resetSession();
-      embeddings.reset()
-      resp.body = json.dumps( { 'response': 'done'} )
+  def on_get(self, req, resp) :
+    session.resetSession();
+    embeddings.reset()
+    resp.body = json.dumps( { 'response': 'done'} )
 
 class RestoreSession:
-    def on_get(self, req, resp) :
-      session.restoreSession();
-      resp.body = json.dumps( { 'response': 'done'} )
+  def on_get(self, req, resp) :
+    session.restoreSession();
+    resp.body = json.dumps( { 'response': 'done'} )
 
 class SaveSession:
-    def on_get(self, req, resp) :
-      session.saveSession();
-      resp.body = json.dumps( { 'response': 'done'} )
+  def on_get(self, req, resp) :
+    session.saveSession();
+    resp.body = json.dumps( { 'response': 'done'} )
 
 
 class Similar:
-    def on_get(self, req, resp, index):
-        the_embeddings = embeddings.getEmbeddings()
-        names = embeddings.nearestNeighbour( the_embeddings[int(index)] ).tolist()
-        resp.body = json.dumps( { 'response' : names } )
+  def on_get(self, req, resp, index):
+    the_embeddings = embeddings.getEmbeddings()
+    names = embeddings.nearestNeighbour( the_embeddings[int(index)] ).tolist()
+    resp.body = json.dumps( { 'response' : names } )
 
 class Difference:
-    def on_get(self, req, resp, positive, negative):
-        the_embeddings = embeddings.getEmbeddings()
-        names = embeddings.nearestNeighbour( the_embeddings[int(positive)] * 2 - the_embeddings[int(negative)] ).tolist()
-        resp.body = json.dumps( { 'response' : names } )
+  def on_get(self, req, resp, positive, negative):
+    the_embeddings = embeddings.getEmbeddings()
+    names = embeddings.nearestNeighbour( the_embeddings[int(positive)] * 2 - the_embeddings[int(negative)] ).tolist()
+    resp.body = json.dumps( { 'response' : names } )
 
 
 
