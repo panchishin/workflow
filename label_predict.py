2bb119d50c3cec4bb98ead40e6cbac316e396991
panchishin <anton@panchishin.com>
2017-08-01 13:36:42 -0400

    label_predict now uses its own session and graph

--- label_predict.py
+++ label_predict.py
@@ -1,10 +1,9 @@
-import session
+import tensorflow as tf
 import random
 import numpy as np
 import label_model
 
 
-sess = session.sess
 
 
 def getRandom(positive_examples,embeddings) :
@@ -13,7 +12,7 @@ def getRandom(positive_examples,embeddings) :
     neg = random.randint(0,embeddings.getEmbeddings().shape[0]-1)
   return neg
 
-def doEpoch(positive_examples,embeddings,model) :
+def doEpoch(positive_examples,embeddings,model,sess) :
   score = 0.0
   for iteration in range(len(positive_examples)) :
     pos = positive_examples[ iteration ]
@@ -25,32 +24,29 @@ def doEpoch(positive_examples,embeddings,model) :
   print ".",
   return score
 
-model = None
-def getModel() :
-  global model
-  if model == None :
-    model = label_model.model()
-  return model
 
-def doTraining(positive_examples,embeddings,model=None) :
-  model = getModel()
-  sess.run(model.init_new_vars_op)
-  target_correct = max( 0.95 , min( 0.99 , 1. - 3. / len(positive_examples) ) )
-  print "Target correct is",target_correct
-  print "Training started",
-  result_correct = 0
-  for _ in range(30) :
-    result_correct = doEpoch(positive_examples,embeddings,model)
-    if result_correct >= target_correct :
-      break
-  print "done training with result correct :",result_correct      
-  return model,sess.run(
-    model.category_out,
-    feed_dict={
-      model.emb_in:embeddings.getEmbeddings(),
-      model.dropout:1.0
-      }
-    )[:,0]
+def doTraining(positive_examples,embeddings) :
+  label_graph = tf.Graph()
+  with label_graph.as_default() :
+    model = label_model.model()
+    with tf.Session(graph=label_graph) as sess :
+      sess.run(model.init_new_vars_op)
+      target_correct = max( 0.95 , min( 0.99 , 1. - 3. / len(positive_examples) ) )
+      print "Target correct is",target_correct
+      print "Training started",
+      result_correct = 0
+      for _ in range(30) :
+        result_correct = doEpoch(positive_examples,embeddings,model,sess)
+        if result_correct >= target_correct :
+          break
+      print "done training with result correct :",result_correct      
+      return model,sess.run(
+        model.category_out,
+        feed_dict={
+          model.emb_in:embeddings.getEmbeddings(),
+          model.dropout:1.0
+          }
+        )[:,0]
 
 def getPredictiveWeights(positive_examples,negative_examples,embeddings) :
   model,weights = doTraining(positive_examples,embeddings)
