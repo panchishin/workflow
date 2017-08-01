70e209fc1a88cca24c1f977c6809f5cf00e54cdf
panchishin <anton@panchishin.com>
2017-08-01 10:11:13 -0400

    make label_model a class for 1st step in modularization

--- label_model.py
+++ label_model.py
@@ -1,27 +1,31 @@
 import tensorflow as tf
 import layer
 
-emb_in = tf.placeholder(tf.float32, [None, 32] , name="emb_in")
-category_in = tf.placeholder(tf.float32, [None, 2] , name="category")
-dropout = tf.placeholder(tf.float32)
+class model:
 
-with tf.variable_scope("layer0") :
-  W0 = layer.weight_variable( [32, 32], name=("layer0_weight") )
-  b0 = layer.bias_variable( [32], name=("layer0_bias") )
-  layer0 = tf.add( tf.matmul(emb_in, W0) , b0 )
-  layer0 = tf.tanh( layer0 )
-  layer0 = tf.nn.dropout( layer0 , dropout )
-
-with tf.variable_scope("layer1") :
-  W1 = layer.weight_variable( [32, 2], name=("layer1_weight") )
-  b1 = layer.bias_variable( [2], name=("layer1_bias") )
-  layer1 = tf.add( tf.matmul(layer0, W1) , b1 )
-
-category_out = tf.nn.softmax( layer1 )
-
-loss = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits( labels=category_in, logits=layer1) )
-train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
-
-correct = tf.reduce_mean( tf.cast( tf.equal( tf.argmax( category_in , 1 ) , tf.argmax( layer1 , 1 ) ) , dtype=tf.float32 ) )
-
-init_new_vars_op = tf.variables_initializer([W0,b0,W1,b1])
+  def __init__(self):
+    self.emb_in = tf.placeholder(tf.float32, [None, 32] , name="emb_in")
+    self.category_in = tf.placeholder(tf.float32, [None, 2] , name="category")
+    self.dropout = tf.placeholder(tf.float32)
+    
+    with tf.variable_scope("layer0") :
+      self.W0 = layer.weight_variable( [32, 32], name=("layer0_weight") )
+      self.b0 = layer.bias_variable( [32], name=("layer0_bias") )
+      self.layer0 = tf.add( tf.matmul(self.emb_in, self.W0) , self.b0 )
+      self.layer0 = tf.tanh( self.layer0 )
+      self.layer0 = tf.nn.dropout( self.layer0 , self.dropout )
+    
+    with tf.variable_scope("layer1") :
+      self.W1 = layer.weight_variable( [32, 2], name=("layer1_weight") )
+      self.b1 = layer.bias_variable( [2], name=("layer1_bias") )
+      self.layer1 = tf.add( tf.matmul(self.layer0, self.W1) , self.b1 )
+    
+    self.category_out = tf.nn.softmax( self.layer1 )
+    
+    self.loss = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits( labels=self.category_in, logits=self.layer1) )
+    self.train = tf.train.GradientDescentOptimizer(0.1).minimize(self.loss)
+    
+    self.correct = tf.reduce_mean( tf.cast( tf.equal( tf.argmax( self.category_in , 1 ) , tf.argmax( self.layer1 , 1 ) ) , dtype=tf.float32 ) )
+    
+    self.init_new_vars_op = tf.variables_initializer([ self.W0, self.b0, self.W1, self.b1 ])
+    
--- label_predict.py
+++ label_predict.py
@@ -13,39 +13,47 @@ def getRandom(positive_examples,embeddings) :
     neg = random.randint(0,embeddings.getEmbeddings().shape[0]-1)
   return neg
 
-def doEpoch(positive_examples,embeddings) :
+def doEpoch(positive_examples,embeddings,model) :
   score = 0.0
   for iteration in range(len(positive_examples)) :
     pos = positive_examples[ iteration ]
     neg = getRandom(positive_examples,embeddings)
-    percent_correct = sess.run(label_model.correct,feed_dict={label_model.emb_in:embeddings.getEmbeddings()[ [pos,neg] ],label_model.category_in:[ [1.,0.],[0.,1.] ],label_model.dropout:1.0})
+    percent_correct = sess.run(model.correct,feed_dict={model.emb_in:embeddings.getEmbeddings()[ [pos,neg] ],model.category_in:[ [1.,0.],[0.,1.] ],model.dropout:1.0})
     score += percent_correct
-    sess.run(label_model.train,feed_dict={label_model.emb_in:embeddings.getEmbeddings()[ [pos,neg] ],label_model.category_in:[ [1.,0.],[0.,1.] ],label_model.dropout:.5})
+    sess.run(model.train,feed_dict={model.emb_in:embeddings.getEmbeddings()[ [pos,neg] ],model.category_in:[ [1.,0.],[0.,1.] ],model.dropout:.5})
   score = 1. * score / len(positive_examples)
   print ".",
   return score
 
-def doTraining(positive_examples,embeddings) :
-  sess.run(label_model.init_new_vars_op)
+model = None
+def getModel() :
+  global model
+  if model == None :
+    model = label_model.model()
+  return model
+
+def doTraining(positive_examples,embeddings,model=None) :
+  model = getModel()
+  sess.run(model.init_new_vars_op)
   target_correct = max( 0.95 , min( 0.99 , 1. - 3. / len(positive_examples) ) )
   print "Target correct is",target_correct
   print "Training started",
   result_correct = 0
   for _ in range(30) :
-    result_correct = doEpoch(positive_examples,embeddings)
+    result_correct = doEpoch(positive_examples,embeddings,model)
     if result_correct >= target_correct :
       break
   print "done training with result correct :",result_correct      
-  return sess.run(
-    label_model.category_out,
+  return model,sess.run(
+    model.category_out,
     feed_dict={
-      label_model.emb_in:embeddings.getEmbeddings(),
-      label_model.dropout:1.0
+      model.emb_in:embeddings.getEmbeddings(),
+      model.dropout:1.0
       }
     )[:,0]
 
 def getPredictiveWeights(positive_examples,negative_examples,embeddings) :
-  weights = doTraining(positive_examples,embeddings)
+  model,weights = doTraining(positive_examples,embeddings)
   for element in positive_examples :
     weights[element] = 1.
   for element in negative_examples :
@@ -57,11 +65,11 @@ def groundTruthReport(positive_examples,embeddings,mnist) :
   category_in_full_set = np.array( [ [1.,0.] if item == 4 else [0.,1.] for item in np.argmax(mnist.test.labels,1) ] )
 
   data = sess.run(
-    label_model.category_out,
+    model.category_out,
     feed_dict={
-      label_model.emb_in:embeddings.getEmbeddings(),
-      label_model.category_in:category_in_full_set,
-      label_model.dropout:1.0
+      model.emb_in:embeddings.getEmbeddings(),
+      model.category_in:category_in_full_set,
+      model.dropout:1.0
       }
     )
 
