5d8a4193f6bda5823285382b12ba26566be89a62
panchishin <anton@panchishin.com>
2017-08-02 15:56:07 -0400

    bug fix.  broke binary classifier

--- label_predict.py
+++ label_predict.py
@@ -6,10 +6,11 @@ import label_model
 
 
 
-def getRandom(positive_examples,embeddings) :
-  neg = positive_examples[0]
-  while neg in positive_examples :
-    neg = random.randint(0,embeddings.getEmbeddings().shape[0]-1)
+def getRandom(examples,embeddings) :
+  neg = random.randint(0,embeddings.getEmbeddings().shape[0]-1)
+  for item_list in examples :
+    if neg in item_list :
+      return getRandom( examples, embeddings )
   return neg
 
 
@@ -22,34 +23,25 @@ def doEpoch(embeddings_in,category_in,model,sess) :
 def doTraining(examples,embeddings) :
   label_graph = tf.Graph()
   with label_graph.as_default() :
-    model = label_model.model(number_of_classes=max(2, len(examples) ) )
+    model = label_model.model(number_of_classes=1+len(examples) )
     with tf.Session(graph=label_graph) as sess :
       sess.run( tf.global_variables_initializer() )
       target_correct = max( 0.95 , min( 0.998 , 1. - 5. / ( len(examples) * len(examples[0]) ) ) )
       print "Target correct is",target_correct
       print "Training started",
       result_correct = 0
-      for epoch_count in range(200) :
+      for epoch_count in range(50) :
 
-        if len(examples) == 1 :
-          negative_examples = [ getRandom(examples[0],embeddings) for item in examples[0]]
+        example_category = []
+        example_embeddings = []
+        identity  = np.identity( len(examples) + 1 )
+        for category in range(len(examples)) :
+          example_category.extend( [ identity[category,:] for item in examples[category] ] )
+          example_embeddings.extend( [ embeddings.getEmbeddings()[ subitem ] for subitem in examples[category] ] )
 
-          # convert the examples and negative examples which are image ids into embeddings
-          example_category = [ [1.,0.] for subitem in examples[0] ]
-          example_embeddings = [ embeddings.getEmbeddings()[ subitem ] for subitem in examples[0] ]
-          negative_category = [ [0.,1.] for subitem in negative_examples ]
-          negative_embeddings = [ embeddings.getEmbeddings()[ subitem ] for subitem in negative_examples ]
-
-          example_embeddings.extend( negative_embeddings )
-          example_category.extend( negative_category )
-
-        if len(examples) > 1 :
-          example_category = []
-          example_embeddings = []
-          identity  = np.identity( len(examples) )
-          for category in range(len(examples)) :
-            example_category.extend( [ identity[category,:] for item in examples[category] ] )
-            example_embeddings.extend( [ embeddings.getEmbeddings()[ subitem ] for subitem in examples[category] ] )
+        negative_examples = [ getRandom(examples,embeddings) for item in examples[0]]
+        example_category.extend( [ identity[-1,:] for subitem in negative_examples ] )
+        example_embeddings.extend( [ embeddings.getEmbeddings()[ subitem ] for subitem in negative_examples ] )
 
         result_correct = doEpoch( example_embeddings, example_category, model, sess )
 
@@ -80,7 +72,7 @@ def predictiveBinaryWeights(positive_examples,negative_examples,embeddings) :
 
 def predictiveMultiClassWeights(examples,embeddings) :
   model,weights = doTraining(examples,embeddings)
-  identity  = np.identity( len(examples) )
+  identity  = np.identity( len(examples) + 1 )
   for category in range(len(examples)) :
     for example in examples[category] :
       weights[example] = identity[category,:]
