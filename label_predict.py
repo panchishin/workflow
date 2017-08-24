27d6e4baa0f4e444c1d5334ee9413a1450ee3578
panchishin <anton@panchishin.com>
2017-08-24 15:24:11 -0400

    More tolerance on error increasing

--- label_predict.py
+++ label_predict.py
@@ -84,7 +84,7 @@ def _doTraining(examples,training_examples,test_examples,embeddings,has_unknown=
 
         best_correct = max( best_correct , test_correct )
 
-        if total_examples >= 20 and epoch_count > 10 and ((1-test_correct) >= (1-best_correct) * 1.2) :
+        if total_examples >= 20 and epoch_count >= 10 and ((1-test_correct) >= (1-best_correct) * 1.5) :
           print "End > test error",
           break
         if total_examples >= 20 and epoch_count > 10 and 1-result_correct < 0.0001 :
