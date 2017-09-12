a27c158fe202cbdb30630a4a2110ac560b1d58b6
panchishin <anton@panchishin.com>
2017-09-12 13:06:28 -0400

    add a test script

---
+++ test.sh
@@ -0,0 +1,18 @@
+echo ========================
+echo python autoencode_model_spec.py
+python autoencode_model_spec.py
+
+echo ========================
+echo git checkout meta-data
+git checkout meta-data
+
+echo ========================
+echo python data_source.py
+python data_source.py
+
+echo ========================
+echo python label_spec.py 
+python label_spec.py 
+
+echo ========================
+echo Done
