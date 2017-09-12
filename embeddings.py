7358c2fa7026b5f1168a94109d2328ec007c66d7
panchishin <anton@panchishin.com>
2017-09-12 13:11:47 -0400

    parameterize variables

--- embeddings.py
+++ embeddings.py
@@ -1,43 +1,37 @@
 import numpy as np
 import random
-import session
 from scipy import spatial
 
 class Embeddings:
-  def __init__(self) :
+  def __init__(self, session=None, model=None, max_search_size=2000) :
     self.data_set = []
     self.all_embeddings = []
-    self.autoencode_model = session.autoencode_model
+    self.max_search_size = max_search_size
+    if session == None :
+      import session as importSession
+      self.session = importSession
+    else :
+      self.session = session
+    if model == None :
+      self.model = self.session.autoencode_model
+    else :
+      self.model = model
 
   def reset(self) :
     self.all_embeddings = []
-  
-  def update(self) :
-    self.all_embeddings = session.sess.run(self.autoencode_model.embedding,
-        feed_dict={self.autoencode_model.x_in:self.data_set} ).reshape([-1,self.autoencode_model.SIZE])
-  
+
   def getEmbeddings(self) :
     if len(self.all_embeddings) == 0 :
-      self.update()
+      self.all_embeddings = self.session.sess.run(self.model.embedding,
+        feed_dict={self.model.x_in:self.data_set} ).reshape([-1,self.model.SIZE])
     return self.all_embeddings
   
-  def calculateDistance(self,index1,index2) :
-    return spatial.distance.cosine( index1, index2 )
-  
-  def nearestNeighbour(self,embedding,size=10) :
-    the_embeddings = self.getEmbeddings()
-    index_list = range(the_embeddings.shape[0])
-    distances = np.array([ self.calculateDistance(embedding,the_embeddings[other]) for other in index_list ])
-    nearest = np.argsort( distances )[:size]
-    return np.array(index_list)[nearest]
-  
   def nearestNeighbourByIndex(self,index,size=10) :
-    max_search_size = 2000
     the_embeddings = self.getEmbeddings()
     embedding = the_embeddings[index]
-    offset = random.randint(0,the_embeddings.shape[0]-max_search_size-1)
-    the_embeddings = the_embeddings[offset:(offset+max_search_size),:]
+    offset = random.randint(0,the_embeddings.shape[0]-self.max_search_size-1)
+    the_embeddings = the_embeddings[offset:(offset+self.max_search_size),:]
     index_list = range(the_embeddings.shape[0])
-    distances = np.array([ self.calculateDistance(embedding,the_embeddings[other]) for other in index_list ])
+    distances = np.array([ spatial.distance.cosine(embedding,the_embeddings[other]) for other in index_list ])
     nearest = np.argsort( distances )[:size]
     return np.array(index_list)[nearest]+offset
